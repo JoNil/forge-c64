e86ab878ed1c58caf2162939010fbea918e8b82c
Jonathan Nilsson <jonathan@voysys.se>
2021-06-02 23:41:17 +0200

    Clear screen

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,22 @@ extern "C" {
     fn __chrout(c: u8);
 }
 
+const SCREEN: *mut [u8; 1000] = 0x0400 as *mut [u8; 1000];
+
+fn clear_screen() {
+    unsafe {
+        let screen = &mut *SCREEN;
+
+        for c in screen {
+            *c = 0x20;
+        }
+    }
+}
+
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "C" fn main() {
+    clear_screen();
     __chrout(b'H');
     __chrout(b'E');
     __chrout(b'L');

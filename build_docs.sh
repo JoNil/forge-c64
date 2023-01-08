a5e7620823d9052c972596063aab38babda4ca97
Jonathan Nilsson <jonathan@voysys.se>
2023-01-08 01:17:48 +0100

    Display size of exe

--- build_docs.sh
+++ build_docs.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker run -it --mount src="$(pwd)",target=/home/mos/forge-c64,type=bind --mount src="$(pwd)/.cargo/git",target=/home/mos/.cargo/git,type=bind --mount src="$(pwd)/.cargo/registry",target=/home/mos/.cargo/registry,type=bind mrkits/rust-mos bash -c "cd forge-c64 && cargo doc"
-docker container prune -f
+docker container prune -f > /dev/null

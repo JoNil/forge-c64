c36bcf2d70404728b0b0b22c63a01ae6e4f70724
Jonathan Nilsson <lord.jonil@gmail.com>
2025-10-19 18:51:54 +0200

    Less bugs

--- src/entity.rs
+++ src/entity.rs
@@ -3,7 +3,8 @@ use core::mem::transmute;
 use crate::{
     map::{MAP, MAP_HEIGHT, MAP_WIDTH},
     tile::{
-        clear_resource, has_resource, is_dir_down, is_dir_left, is_dir_right, is_dir_up, set_resource, DIR_MASK, TILE_DIR_DOWN, TILE_DIR_LEFT, TILE_DIR_RIGHT, TILE_DIR_UP
+        clear_resource, has_resource, is_dir_down, is_dir_left, is_dir_right, is_dir_up,
+        set_resource,
     },
     tileset::TILESET,
 };
@@ -34,27 +35,25 @@ pub unsafe fn update_entites() {
         let tile = clear_resource(MAP[map_i]);
         MAP[map_i] = tile;
 
-        let tile_dir = tile & DIR_MASK;
-
-        if tile != 0 && tile_dir == TILE_DIR_DOWN {
+        if is_dir_down(tile) {
             y += 1;
             map_i += MAP_WIDTH as usize;
 
             entities.y[i] = y;
             entities.i[i] = map_i;
-        } else if tile_dir == TILE_DIR_UP {
+        } else if is_dir_up(tile) {
             y -= 1;
             map_i -= MAP_WIDTH as usize;
 
             entities.y[i] = y;
             entities.i[i] = map_i;
-        } else if tile_dir == TILE_DIR_LEFT {
+        } else if is_dir_left(tile) {
             x -= 1;
             map_i -= 1;
 
             entities.x[i] = x;
             entities.i[i] = map_i;
-        } else if tile_dir == TILE_DIR_RIGHT {
+        } else if is_dir_right(tile) {
             x += 1;
             map_i += 1;
 
--- src/tile.rs
+++ src/tile.rs
@@ -1,25 +1,19 @@
-pub const RESOURCE_BIT: u8 = 0x10;
-pub const DIR_MASK: u8 = 0b11;
-
-pub const TILE_DIR_DOWN: u8 = 0b00;
-pub const TILE_DIR_LEFT: u8 = 0b01;
-pub const TILE_DIR_UP: u8 = 0b10;
-pub const TILE_DIR_RIGHT: u8 = 0b11;
-
-pub const fn is_dir_down(tile: u8) -> bool {
-    tile != 0 && (tile & DIR_MASK) == TILE_DIR_DOWN
-}
+const RESOURCE_BIT: u8 = 0x10;
 
 pub const fn is_dir_left(tile: u8) -> bool {
-    (tile & DIR_MASK) == TILE_DIR_LEFT
+    tile == 1 || tile == 5 || tile == 9
 }
 
 pub const fn is_dir_up(tile: u8) -> bool {
-    (tile & DIR_MASK) == TILE_DIR_UP
+    tile == 2 || tile == 6 || tile == 10
 }
 
 pub const fn is_dir_right(tile: u8) -> bool {
-    (tile & DIR_MASK) == TILE_DIR_RIGHT
+    tile == 3 || tile == 7 || tile == 11
+}
+
+pub const fn is_dir_down(tile: u8) -> bool {
+    tile == 4 || tile == 8 || tile == 12
 }
 
 pub const fn has_resource(tile: u8) -> bool {
